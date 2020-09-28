bb743556d31b4a116df3f474af3f5b08829481cf
kate <kan004@ucr.edu>
2020-09-27 19:24:35 -0700

    added data table, header, main, nav, search name, wrapper components

---
+++ reactapp/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on heading to filter or use the search box</p>
+        </div>
+    )
+}
+
+export default Header;
