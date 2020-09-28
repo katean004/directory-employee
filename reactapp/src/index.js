cee7b21456cfd1ec967f34c53ef25774c73bf83f
kate <kan004@ucr.edu>
2020-09-27 18:25:47 -0700

    updated index js

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,10 +1,12 @@
+  
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//change unregister() to register() below for offline function. 
+serviceWorker.unregister();
 
