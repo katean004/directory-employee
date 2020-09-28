22609ced2adbb56a1b90e23af2b346c8fb88f845
kate <kan004@ucr.edu>
2020-09-27 19:47:36 -0700

    Updates

---
+++ precache-manifest.7cc22902fa8e699be6b5ab74f9b27088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03fd7ead7c610723225cbd34c7b0519",
+    "url": "/directory-employee/index.html"
+  },
+  {
+    "revision": "69db5c3157a9910b6518",
+    "url": "/directory-employee/static/css/main.7112d038.chunk.css"
+  },
+  {
+    "revision": "bd3c54d028a235040ce9",
+    "url": "/directory-employee/static/js/2.97890e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory-employee/static/js/2.97890e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69db5c3157a9910b6518",
+    "url": "/directory-employee/static/js/main.65160485.chunk.js"
+  },
+  {
+    "revision": "8cf8c8b650efdfd1287f",
+    "url": "/directory-employee/static/js/runtime-main.a616047a.js"
+  }
+]);
