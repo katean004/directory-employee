3dd93eeb172d029091286181dcc43dca7c50151f
kate <kan004@ucr.edu>
2020-09-27 19:00:06 -0700

    updated app css

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
+import Main from './components/Main';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
 import "./App.css";
-import Header from "./components/Header/index.js";
-
-
-
+  
 function App() {
     return (
-        <div>
+      <div className="App">
+        <Wrapper>
             <Header />
-        </div>
+            <Main />
+        </Wrapper>
+      </div>
     );
   }
   
   export default App;
-  
